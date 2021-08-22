b074c2e6bd3b13cbe8d6b32a550abcd71396d82c
Yash Poojary <yashspoojary245@gmail.com>
2021-08-22 19:21:08 +0530

    Initial Commit

---
+++ Shared/EmojiArtGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtGameApp.swift
+//  Shared
+//
+//  Created by Yash Poojary on 22/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
